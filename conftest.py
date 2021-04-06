9d3cfbf63ca3a8ca0104c5d1db5d850bc0ed2bc7
Vladislav Liubasiuk <liubasiuk.v@gmail.com>
2021-04-06 19:25:10 +0300

    part 5, for god sake I made it right, at last!

--- conftest.py
+++ conftest.py
@@ -3,40 +3,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--brs_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default=None,
-                     help="Choose language: en or any other supported")
+    parser.addoption("--language", action = "store", default = None,
+                     help = "Need language")
 
 @pytest.fixture(scope="function")
 def browser(request):
-
-    browser_name = request.config.getoption("brs_name")
     user_language = request.config.getoption("language")
-    browser = None
-    
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        #browser = webdriver.Chrome()
-        
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        browser = webdriver.Chrome(options=options)
-        
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        #browser = webdriver.Firefox()
-        
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(firefox_profile=fp)
-        
-    else:
-        raise pytest.UsageError("--brs_name should be chrome or firefox")
+    options = Options()
+    options.add_experimental_option("prefs", {"intl.accept_languages": user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
-
+    
 #Есть одна важная особенность поведения конфигурационных файлов, о которой вы обязательно должны знать.
 #PyTest автоматически находит и подгружает файлы conftest.py, которые находятся в директории с тестами. 
 #Если вы храните все свои скрипты для курса в одной директории, будьте аккуратны и следите, чтобы не возникало ситуации, 
@@ -56,4 +35,3 @@ def browser(request):
 #│   └── conftest.py
 #│   └── test_main_page.py
 #правильно!
-
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,15 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from pages.login_page import LoginPage
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        alert = self.browser.switch_to.alert #failsafe если вдруг появиться алерт? (тест упадет так как метод не обрабативает их)
+        alert.accept()
+        #return LoginPage(browser=self.browser, url=self.browser.current_url)
         
     def should_be_login_link(self):
         self.browser.find_element(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
